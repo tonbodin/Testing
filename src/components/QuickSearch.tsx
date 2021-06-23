5f7c498f2b3b2ab7ccf2d009697730ab548900cc
tonbodin <tonbodin@hotmail.com>
2021-06-23 16:45:30 -0700

    more buttons

--- src/components/QuickSearch.tsx
+++ src/components/QuickSearch.tsx
@@ -46,6 +46,14 @@ export const Main = styled(Box as any)`
   -ms-user-select: none;
 `
 
+function highlightBackground(e: any) {
+  e.target.style.backgroundColor = "#2A2E39"
+}
+
+function unhighlightBackground(e: any) {
+  e.target.style.backgroundColor = "#1f2436"
+}
+
 export const FilterHeading = styled(Heading as any)`
   margin-bottom: 0.4em;
   color: ${theme.colors.text1};
@@ -92,45 +100,54 @@ export const QuickSearch: React.FC<{
         pr="xxlarge"
       >
         <Group>
-          <FilterHeading as="h6">Has Description</FilterHeading>
-          <ButtonGroup value={hasDescription} onChange={setHasDescription}>
-            <ButtonItem value={"yes"}>Yes</ButtonItem>
-            <ButtonItem value={"no"}>No</ButtonItem>
+          <FilterHeading as="h4" >Has Description</FilterHeading>
+          <ButtonGroup value={hasDescription} onChange={setHasDescription} >
+            <ButtonItem value={"yes"} onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>Yes</ButtonItem>
+            <ButtonItem value={"no"} onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>No</ButtonItem>
           </ButtonGroup>
         </Group>
 
         <Group>
-          <FilterHeading as="h6">Fields</FilterHeading>
+          <FilterHeading as="h4">Fields</FilterHeading>
           <ButtonGroup value={fieldTypes} onChange={setFieldTypes}>
-            <ButtonItem value="dimensions">Dimension</ButtonItem>
-            <ButtonItem value="measures">Measure</ButtonItem>
+            <ButtonItem value="dimensions" onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>Dimension</ButtonItem>
+            <ButtonItem value="measures" onMouseEnter={highlightBackground}
+            onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>Measure</ButtonItem>
           </ButtonGroup>
         </Group>
 
         <Group>
-          <FilterHeading as="h6">Has Tags</FilterHeading>
+          <FilterHeading as="h4">Has Tags</FilterHeading>
           <ButtonGroup value={hasTags} onChange={setHasTags}>
-            <ButtonItem value="yes">Yes</ButtonItem>
-            <ButtonItem value="no">No</ButtonItem>
+            <ButtonItem value="yes" onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>Yes</ButtonItem>
+            <ButtonItem value="no" onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>No</ButtonItem>
           </ButtonGroup>
         </Group>
 
         {showComments && (
           <Group>
-            <FilterHeading as="h6">Has Comments</FilterHeading>
+            <FilterHeading as="h4">Has Comments</FilterHeading>
             <ButtonGroup value={hasComments} onChange={setHasComments}>
-              <ButtonItem value="yes">Yes</ButtonItem>
-              <ButtonItem value="no">No</ButtonItem>
+              <ButtonItem value="yes" onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>Yes</ButtonItem>
+              <ButtonItem value="no" onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>No</ButtonItem>
             </ButtonGroup>
           </Group>
         )}
 
         <Group>
-          <FilterHeading as="h6">Type</FilterHeading>
+          <FilterHeading as="h4">Type</FilterHeading>
           <ButtonGroup value={selectedFields} onChange={setSelectedFields}>
             {fields.map(field => {
               return (
-                <ButtonItem key={field} value={field}>
+                <ButtonItem key={field} value={field} onMouseEnter={highlightBackground}
+                onMouseLeave={unhighlightBackground} style={{ backgroundColor: "#1f2436", borderColor: "#9B9EA3", color: "#9B9EA3" }}>
                   {field}
                 </ButtonItem>
               )
